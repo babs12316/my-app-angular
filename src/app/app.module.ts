405ffc7e8a9ac2fd46e27bb277816a48251ca9fe
babs12316 <44439609+babs12316@users.noreply.github.com>
2019-09-24 16:51:30 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopNavComponent } from './product-list/top-nav/top-nav.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopNavComponent,
+   
   ],
   imports: [
     BrowserModule,
